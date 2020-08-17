55ebed7a8d9e77d6a3f8886bec0d250f3eac2435
unknown <estarg51@gmail.com>
2020-08-18 01:17:38 +0900

    Added a file about me

---
+++ 2020.08.18.js
@@ -0,0 +1,10 @@
+var leephoter = {
+    name : 'Hangyeol Lee',
+    food : function food() {
+        console.log('Pork cutlet !!')
+    },
+}
+console.log(leephoter)
+console.log(leephoter.name)
+console.log(leephoter['name'])
+console.log(leephoter.food)
