59ab492dac8450fa57917defff65c3dc80f37bdc
Han Gyoel Lee <estarg51@gmail.com>
2020-09-05 02:43:13 +0900

    손아프지만

---
+++ 2020.09.05.js
@@ -0,0 +1,3 @@
+console.log("맥북 프로 산 기념... 오른 손 목은 언제 다 나으려나")
+console.log("What do you think of this Macbook Pro's price and value for me ?? I don't know yet how it will be changed to my level of Programming")
+console.log("hhhahaahhhha")
