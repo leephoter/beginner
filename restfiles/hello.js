6ab61cb3ff77055584faa78356046568f212cdea
Han Gyoel Lee <estarg51@gmail.com>
2020-09-06 01:26:02 +0900

    ??

--- restfiles/hello.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World");
-console.log("2020.08.16");
-console.log('2020.08.17')
--- restfiles/zerocho.js
+++
@@ -1,4 +0,0 @@
-console.log("힘들다");
-console.log("어쩔 수 없다 한결아...");
-console.log('2020.08.16 Start')
-console.log('2020.08.17')
