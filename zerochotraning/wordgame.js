e6913785c0d190448a09a97c3b61f21c50bd1403
Han Gyoel Lee <estarg51@gmail.com>
2020-09-06 23:35:53 +0900

    파일정리

---
+++ zerochotraning/wordgame.js
@@ -0,0 +1,9 @@
+var word = prompt("Let's Play the Word Game !!")
+for (var answer = prompt(word); word[word.length - 1] === answer[0]; ) {
+    if (word[word.length - 1] === answer[0]) {
+        alert('nice')
+        word = answer
+        var answer = prompt(word)
+    }
+} alert('U lost !')
+
