1809bfbb3a8e4c19c37fb616ef2611bc4590e563
Han Gyoel Lee <estarg51@gmail.com>
2020-09-07 01:07:26 +0900

    Modify the restfile

--- restfiles/20.09.05.js
+++ restfiles/20.09.05.js
@@ -2,4 +2,3 @@ console.log("맥북 프로 산 기념... 오른 손 목은 언제 다 나으려�
 console.log("What do you think of this Macbook Pro's price and value for me ?? I don't know yet how it will be changed to my level of Programming")
 console.log("너무 오랜만에 해서 그런지 커밋이 안된다ㅠㅠ")
 console.log("삭제한 파일은 Source Control에서도 삭제를 한 후 커밋을 날리자")
-console.log("zerochotraning에 았던 파일들이 Git에서 사라졌음 ㅠㅠ")
