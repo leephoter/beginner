1e8fdbcdced3d252cba0ac16ba5add872149a549
Han Gyoel Lee <estarg51@gmail.com>
2020-09-11 02:44:33 +0900

    Added the Tictactoe game

--- zrchtraning/20.09.05-tictactoe.js
+++ zrchtraning/20.09.05-tictactoe.js
@@ -1,18 +1,39 @@
-var frame = document.body;
-var tabble = document.createElement('table');
-var lines = []
-var spaces = []
+var frame = document.body; //frame = 바디
+var tabble = document.createElement('table'); //tabble = 테이블
+var lines = [] //linse = 줄들
+var spaces = [] //spaces = 칸들
+var turn = "X"
 
-var passivecallback = function(ebent) {
-    console.log(ebent.target)
+var passivecallback = function(ebent) { //passivecallback = 비동기콜백
+    console.log(ebent.target) //ebent = 이벤트
+    console.log(ebent.target.parentNode)
+    console.log(ebent.target.parentNode.parentNode)
+
+    var lineth = lines.indexOf(ebent.target.parentNode)
+    console.log('lineth', lineth)
+    var spaceth = spaces[lineth].indexOf(ebent.target)
+    console.log('spaceth', spaceth)
+
+    if (spaces[lineth][spaceth].textContent !== "") { // 칸이 이미 채워져 있는가 
+        console.log("It's no blank")
+        } else {
+        console.log("It's blank")
+        spaces[lineth][spaceth].textContent = turn; // 칸들[몇줄][몇칸]에 turn이라는 text 삽입
+        if (turn === "X") {
+            turn = "O" 
+        } else {
+            turn = "X"
+        }
+    }
 }
 
+
 for (var i = 1; i <= 3; i++) {
-    var line = document.createElement('tr')
+    var line = document.createElement('tr') //line = 줄
     lines.push(line)
     spaces.push([])
-    for (var j = 1; j <= 3; j += 1) {
-        var space = document.createElement('td')
+    for (var j = 1; j <= 3; j += 1) { 
+        var space = document.createElement('td') //space = 칸
         space.addEventListener('click', passivecallback)
         spaces[i - 1].push(space)
         line.appendChild(space)
@@ -20,4 +41,4 @@ for (var i = 1; i <= 3; i++) {
     tabble.appendChild(line)
 }
 frame.appendChild(tabble)
-console.log(lines, spaces)
+console.log('lines', lines, 'spaces', spaces)
