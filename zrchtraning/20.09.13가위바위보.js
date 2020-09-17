3ff9b30530141737cfadcd965e45029c4e8c974f
Han Gyoel Lee <estarg51@gmail.com>
2020-09-17 19:27:52 +0900

    가위바위보 개어려움

--- zrchtraning/20.09.13가위바위보.js
+++ zrchtraning/20.09.13가위바위보.js
@@ -1,8 +1,57 @@
-var left = 0;
-setInterval(() => {
-    var left = 0;
-    document.querySelector('#computer').style.background = 'url(200915.png)' + left + '0';
-}, 1000);
-setTimeout(
+var imageaddress = 0;
+var rps = { // Key:value 1:1matcing
+    rock: '0',
+    scissors: '-290px',
+    paper: '-580px',
+}
+console.log(Object.entries(rps));
+function computerpic(imageaddress) {
+    return Object.entries(rps).find(function(v) {
+        return v[1] === imageaddress
+    })[0];
+}
 
-) 
+setInterval(function () {
+    if (imageaddress === rps.rock) {
+        imageaddress = rps.scissors;
+    }
+    else if (imageaddress === rps.scissors) {
+        imageaddress = rps.paper;
+    }
+    else {
+        imageaddress = rps.rock;
+    }
+    document.querySelector('#computer').style.background = 'url(200915.png)' + imageaddress + '0';
+}, 100);
+
+document.querySelectorAll('.btn').forEach(function(btn) {
+    btn.addEventListener('click', function () {
+        var mypic = this.textContent
+        console.log(mypic, computerpic(imageaddress));
+        if (mypic === 'scissors') {
+            if (computerpic(imageaddress) === 'scissors') {
+                console.log("Draw !!")
+            } else if (computerpic(imageaddress) === 'rock'){
+                console.log("U Lost !!")
+            } else {
+                console.log("U Win !!")
+            }
+        } else if (mypic === 'rock') {
+            if (computerpic(imageaddress) === 'rock') {
+                console.log("Draw !!")
+            } else if (computerpic(imageaddress) === 'paper'){
+                console.log("U Lost !!")
+            } else {
+                console.log("U Win !!")
+            }
+        } else {
+            if (computerpic(imageaddress) === 'paper') {
+                console.log("Draw !!")
+            } else if (computerpic(imageaddress) === 'scissors'){
+                console.log("U Lost !!")
+            } else {
+                console.log("U Win !!")
+            }
+        }
+    })
+})
