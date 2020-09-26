5c53d1a501d4b8110dc25cf1efbc10359d18f0c2
Han Gyoel Lee <estarg51@gmail.com>
2020-09-26 19:14:18 +0900

    가위바위보 거의 다 끝남

--- zrchtraning/20.09.13가위바위보.js
+++ zrchtraning/20.09.13가위바위보.js
@@ -7,56 +7,52 @@ var rps = { // Key:value 1:1matcing
 //console.log(Object.entries(rps));
 function computerpic(imageaddress) {
     return Object.entries(rps).find(function(v) { //find는 배열에서만 가능한 함수이기 때문에 Object.entries(객체) -> 객체를 배열의 형태로 만드르어줌
-        console.log(v)
         return v[1] === imageaddress // 다시
     })[0]; // 다시 
 }
 //비동기
-setInterval(function () {
-    if (imageaddress === rps.rock) {
-        imageaddress = rps.scissors;
-    }
-    else if (imageaddress === rps.scissors) {
-        imageaddress = rps.paper;
-    }
-    else {
-        imageaddress = rps.rock;
-    }
-    document.querySelector('#computer').style.background = ' url(200915.png) ' + imageaddress + ' 0';
-}, 300);
-var elements = document.querySelectorAll('.btn');
+var intervall;
+function intervalmaker() {
+    intervall = setInterval(function () {
+        if (imageaddress === rps.rock) {
+            imageaddress = rps.scissors;
+        }
+        else if (imageaddress === rps.scissors) {
+            imageaddress = rps.paper;
+        }
+        else {
+            imageaddress = rps.rock;
+        }
+        document.querySelector('#computer').style.background = ' url(200915.png) ' + imageaddress + ' 0';
+    }, 120);
+}
+intervalmaker();
+// var elements = document.querySelectorAll('.btn');
 
-for (var i = 0, element; (element = elements[i]); i++) {
-    console.log(element);
+// for (var i = 0, element; (element = elements[i]); i++) {
+//     console.log(element);
+// }
+// 점수표
+var scoreline = {
+    scissors: 1,
+    rock: 0,
+    paper: -1,
 }
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function () { 
+        clearInterval(intervall)
+        setTimeout(function() {
+            intervalmaker()
+        }, 2000);
         var mypic = this.textContent
-        console.log(mypic, computerpic(imageaddress));
-        if (mypic === 'scissors') {
-            if (computerpic(imageaddress) === 'scissors') {
-                console.log("Draw !!")
-            } else if (computerpic(imageaddress) === 'rock'){
-                console.log("U Lost !!")
-            } else {
-                console.log("U Win !!")
-            }
-        } else if (mypic === 'rock') {
-            if (computerpic(imageaddress) === 'rock') {
-                console.log("Draw !!")
-            } else if (computerpic(imageaddress) === 'paper'){
-                console.log("U Lost !!")
-            } else {
-                console.log("U Win !!")
-            }
-        } else {
-            if (computerpic(imageaddress) === 'paper') {
-                console.log("Draw !!")
-            } else if (computerpic(imageaddress) === 'scissors'){
-                console.log("U Lost !!")
-            } else {
-                console.log("U Win !!")
-            }
+        if (scoreline[mypic] - scoreline[co mputerpic(imageaddress)] === 0) {
+            console.log("Draw !!")
+        }
+        else if ([-1, 0].includes(scoreline[mypic] - scoreline[computerpic(imageaddress)])) {
+            console.log("U Win !!")
+        }
+        else {
+            console.log("U Lost !!")
         }
     })
 })
