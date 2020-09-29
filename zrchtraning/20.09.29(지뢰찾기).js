c8665c61008a6b96a8c05ea55e469840e3eab191
Han Gyoel Lee <estarg51@gmail.com>
2020-09-29 17:54:13 +0900

    value가 안됨;;;;

---
+++ zrchtraning/20.09.29(지뢰찾기).js
@@ -0,0 +1,6 @@
+document.querySelector('#exec').addEventListener('click', function() {
+    var hor = parseInt(document.querySelector('#hor').value);
+    var ver = parseInt(document.querySelector('#ver').value);
+    var mine = parseInt(document.querySelector('#mine').value);
+    console.log(hor, ver, mine);
+});
