f71c4fc9654d95fa3b9f6b24d5f81c8bbf2c404a
Han Gyoel Lee <estarg51@gmail.com>
2020-10-05 18:41:35 +0900

    요호호

--- zrchtraning/20.09.29(지뢰찾기).js
+++ zrchtraning/20.09.29(지뢰찾기).js
@@ -1,6 +1,21 @@
 document.querySelector('#exec').addEventListener('click', function() {
-    var hor = parseInt(document.querySelector('#hor').value);
-    var ver = parseInt(document.querySelector('#ver').value);
-    var mine = parseInt(document.querySelector('#mine').value);
+    var hor = parseInt(document.querySelector('#hor').value)
+    var ver = parseInt(document.querySelector('#ver').value)
+    var mine = parseInt(document.querySelector('#mine').value)
     console.log(hor, ver, mine);
+
+    var dataset = [];
+    var tbody = document.querySelector('#table tbody');
+    for (var i = 0; i < ver; i += 1) {
+        var arr = [];
+        dataset.push(arr);
+        for (var j = 0; j < hor; j += 1) {
+            arr.push(1);
+        //     var td = document.createElement('td');
+        //     tr.appendChild(td);
+        // }
+        // tbody.appendChild(tr);
+        }
+    }
+    console.log(dataset);
 });
