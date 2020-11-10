0bb6a583055603aaf24117dae633dafdb51b0aaa
Han Gyoel Lee <estarg51@gmail.com>
2020-11-11 02:21:32 +0900

    요호호.......

--- zrchtraning/2020.11.10(지뢰다시).js
+++ zrchtraning/2020.11.10(지뢰다시).js
@@ -5,14 +5,14 @@ document.querySelector('#exec').addEventListener('click', function() {
     console.log(hor, ver, mine);
     
     var dataset = [];
-    var tbody = document.querySelector('#tabel tbody');
+    var tbody = document.querySelector('#table tbody');
     for (var i = 0; i < ver; i+=1){
         var arr = [];
-        var tr = document.createElement('tr');
+        var tr = document.createElement("tr");
         dataset.push(arr);
         for (var j = 0; j < hor; j+=1) {
             arr.push(1);
-            var td = document.createElement('td');
+            var td = document.createElement("td");
             tr.appendChild(td);
             td.textContent = 1;
         }
