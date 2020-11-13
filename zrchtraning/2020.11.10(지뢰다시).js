a05c176c8dfdc5f75671b1a426d60025799ce560
Han Gyoel Lee <estarg51@gmail.com>
2020-11-13 19:39:35 +0900

    s8-4

--- zrchtraning/2020.11.10(지뢰다시).js
+++ zrchtraning/2020.11.10(지뢰다시).js
@@ -1,13 +1,17 @@
+var tbody = document.querySelector('#table tbody');
+var dataset = [];
 document.querySelector('#exec').addEventListener('click', function() {
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
     // console.log(hor, ver, mine);
+
     //지뢰 위치 뽑기
     var subs = Array(hor * ver).fill().map(function (urea, index) {
         return index;
     });
     var shuffle = []; // 빈 배열
+
     while (subs.length > 80) {
         var movalue = subs.splice(Math.floor(Math.random() * subs.length), 1)[0];
         shuffle.push(movalue);
@@ -15,8 +19,7 @@ document.querySelector('#exec').addEventListener('click', function() {
 
     console.log(shuffle);
     //지뢰 테이블 만들기
-    var dataset = [];
-    var tbody = document.querySelector('#table tbody');
+    
     for (var i = 0; i < ver; i+=1){
         var arr = [];
         var tr = document.createElement('tr');
@@ -24,6 +27,16 @@ document.querySelector('#exec').addEventListener('click', function() {
         for (var j = 0; j < hor; j+=1) {
             arr.push(1);
             var td = document.createElement('td');
+            td.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                var prtr = e.currentTarget.parentNode;
+                var prtbody = e.currentTarget.parentNode.parentNode;
+                var space = Array.prototype.indexOf.call(prtr.children, e.currentTarget);
+                var lines = Array.prototype.indexOf.call(prtbody.children, prtr);
+                console.log(e.currentTarget, space, lines);
+                e.currentTarget.textContent = '!'; //지뢰인것같은 곳을 우클릭으로 "!"표시
+                dataset[lines][space] = '!'; //화면과 데이터를 일치시킴
+            });
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
