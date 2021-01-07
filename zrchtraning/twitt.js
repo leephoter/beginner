c88734f4b8f8c4c84edfa4227158494c8eb58677
LeePhoter <estarg51@gmail.com>
2021-01-08 00:15:03 +0900

    호호호

--- zrchtraning/twitt.js
+++ zrchtraning/twitt.js
@@ -1,7 +1,9 @@
 var inputID = document.getElementById('inputID'); //html의 ID입력창 요소
 var inputComment = document.getElementById('inputComment'); //html의 comment입력창 요소
-var newspace; //twitt이 생성될 공강
-var newspaces = []; //생성된 twitt 배열 (모임)
+var newspace; //twitt이 생성될 공간
+var newTwitt = []; //생성된 twitt 배열 (모임)
+
+var time;
 var randomID = {
   //랜덤 댓글 ID
   1: 'Lee',
@@ -18,77 +20,122 @@ var randomComment = {
   4: '놀지도 마',
   5: '집밥만 먹어',
 };
+var newBox = document.getElementById('newBox'); // twitt span이 생성될 박스
+var newID = []; //입력된 ID 배열
+var newComment = []; // 입력된 comment 배열
+var twitt = document.querySelector('.commentButton'); //twitt 버튼 요소
+var retouchDiv = document.createElement('div'); //수정 버튼 생성
+var retouchButton;
+////// var retouch;
+var removeDiv = document.createElement('div'); //삭제 버튼 생성
+var removeButton;
+/////// var remove;
 
-console.log(
-  randomID[[1, 2, 3, 4, 5].sort(() => 0.5 - Math.random())[0]],
-  randomComment[[1, 2, 3, 4, 5].sort(() => 0.5 - Math.random())[0]],
-);
-//랜덤 코멘트 설정 끝났고 1. 대입 2. 최신 트윗 위로오게 순서 까지 완성하자
-
-function addspace() {
-  newspace = document.createElement('span');
+function addspaceRe() {
+  //div 생성
+  newspace = document.createElement('div');
   newspace.style.display = 'inline-block';
   newspace.style.margin = '3px 3px';
   newspace.style.backgroundColor = 'yellow';
-  return newspaces.unshift(newspace);
+  newspace.style.textAlign = 'right';
+  newspace.style.padding = '10px 10px';
+  newspace.style.fontSize = '14px';
+  newspace.style.width = '115px';
+  newspace.style.height = '60px';
+
+  removeButton = document.createElement('button'); //삭제버튼
+  removeButton.style.width = '40px';
+  removeButton.style.height = '20px';
+  removeButton.style.borderRadius = '20%';
+  removeButton.style.borderStyle = 'outset';
+  removeButton.style.float = 'left';
+  removeButton.innerHTML = '삭제';
+  newspace.appendChild(removeButton);
+  //////////// remove = document.querySelector('')
+
+  retouchButton = document.createElement('button'); //수정버튼
+  retouchButton.style.width = '40px';
+  retouchButton.style.height = '20px';
+  retouchButton.style.borderRadius = '20%';
+  retouchButton.style.borderStyle = 'outset';
+  retouchButton.style.float = 'left';
+  retouchButton.style.marginRight = '5px';
+  retouchButton.innerHTML = '수정';
+  retouchDiv.appendChild(retouchButton);
+  return newTwitt.unshift(newspace), removeButton, retouchButton; //twitt(ID, comment)들의 배열
 }
-var newBox = document.getElementById('newBox'); // twitt span이 생성될 박스
-var newID = []; //
-var newComment = [];
-document.get;
-var twitt = document.querySelector('.commentButton');
+function addspaceTwitt() {
+  //div 생성
+  newspace = document.createElement('div');
+  newspace.style.display = 'inline-block';
+  newspace.style.margin = '3px 3px';
+  newspace.style.backgroundColor = 'yellow';
+  newspace.style.textAlign = 'left';
+  newspace.style.padding = '3px 10px';
+  newspace.style.fontSize = '15px';
+  newspace.style.width = '85%';
+  newspace.style.height = '85%';
+  return newTwitt.unshift(newspace); //twitt(ID, comment)들의 배열
+}
+
+function timetable() {
+  //시간 계산
+  return (
+    new Date().getFullYear() +
+    '-' +
+    (new Date().getMonth() + 1) +
+    '-' +
+    new Date().getDate() +
+    ' ' +
+    new Date().getHours() +
+    '시'
+  );
+}
 
 twitt.addEventListener('click', function () {
-  addspace();
-  addspace();
-  var news0 = newspaces[0]; //
-  var news1 = newspaces[1];
+  //twitt을 누르면
+
+  newID.unshift(inputID.value); //ID 값을 배열앞에 저장
+  newComment.unshift(inputComment.value); //comment 값을 배열앞에 저장
+  inputID.value = ''; //칸 비우기
+  inputComment.value = ''; //칸 비우기
+
+  //ID, comment --> newTwitt[1], news1
+  addspaceRe();
+  var news1;
+  news1 = newTwitt[0];
+  news1.appendChild(retouchDiv);
   newBox.prepend(news1);
-  newBox.prepend(news0);
-  // newBox.appendChild(news0);
-  // newBox.appendChild(news1);
-  newID.unshift(inputID.value); //ID 값을 저장
-  newComment.unshift(inputComment.value); //comment 값 저장
-  inputID.value = ''; //칸 비우는 방법을
-  inputComment.value = ''; //value 말고 textContent로 하는법을 배웠었는데 이용을 못 하겠습니다ㅠㅠ
 
-  news0.style.textAlign = 'left';
-  news0.style.padding = '3px 10px';
-  news0.style.fontSize = '15px';
-  news0.style.width = '85%';
-  news0.style.height = '85%';
-
-  news1.style.textAlign = 'right';
-  news1.style.padding = '10px 10px';
-  news1.style.fontSize = '14px';
-  news1.style.width = '115px';
-  news1.style.height = '60px';
   if (newID[0] === '' && newComment[0] === '') {
-    newspaces[0].innerHTML =
+    news1.innerHTML = timetable();
+    news1.appendChild(retouchButton);
+    news1.appendChild(removeButton);
+  } else {
+    news1.innerHTML = timetable();
+  }
+
+  //date, rebutton
+  addspaceTwitt();
+  var news0;
+  news0 = newTwitt[0];
+  newBox.prepend(news0);
+
+  if (newID[0] === '' && newComment[0] === '') {
+    news0.innerHTML =
       randomID[[1, 2, 3, 4, 5].sort(() => 0.5 - Math.random())[0]] +
       '<br/><br/>' +
       randomComment[[1, 2, 3, 4, 5].sort(() => 0.5 - Math.random())[0]];
-    newspaces[1].innerHTML =
-      new Date().getFullYear() +
-      '-' +
-      (new Date().getMonth() + 1) +
-      '-' +
-      new Date().getDate() +
-      ' ' +
-      new Date().getHours() +
-      '시';
   } else {
-    newspaces[0].innerHTML = newID[0] + '<br/><br/>' + newComment[0];
-    newspaces[1].innerHTML =
-      new Date().getFullYear() +
-      '-' +
-      (new Date().getMonth() + 1) +
-      '-' +
-      new Date().getDate() +
-      ' ' +
-      new Date().getHours() +
-      '시';
+    news0.innerHTML = newID[0] + '<br/><br/>' + newComment[0];
   }
+  console.log(newTwitt);
 });
 
-//삭제 기능, 필터
+// .addEventListener('click', function () {
+//   console.log(20);
+// });
+
+// removeButton.addEventListener('click', function () {
+//   console.log(newTwitt);
+// });
