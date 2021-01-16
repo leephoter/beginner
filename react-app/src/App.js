1902df241fcb04505362ff26b15be8947c8214a4
LeePhoter <estarg51@gmail.com>
2021-01-17 00:50:01 +0900

    react. bind

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        //Component를 초기화시켜주고싶은 코드는 constructor안에 작성
+        super(props);
+        this.state = {
+            mode: "read",
+            subject: {
+                title: "WEB",
+                sub: "World Wide Web !",
+            },
+            welcome: { title: "welcome", desc: "Hello, react!!" },
+            contents: [
+                { id: 1, title: "HTML", desc: "HTML is for information" },
+                { id: 1, title: "CSS", desc: "CSS is for design" },
+                {
+                    id: 1,
+                    title: "JavaScript",
+                    desc: "JavaScript is for interactive",
+                },
+            ],
+        };
+    }
+    render() {
+        console.log("App lender");
+        var _title,
+            _desc = null;
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === "read") {
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
+        console.log("render", this);
+        return (
+            <div className="App">
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                    onChangePage={function () {
+                        this.setState({ mode: "welcome" });
+                    }.bind(this)}
+                ></Subject>
+                <TOC data={this.state.contents}></TOC>
+                <Content title={_title} desc={_desc}></Content>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ react-app/src/components/Subject.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+class Subject extends Component {
+    render() {
+        return (
+            //Component는 하나의 최상위 태그만 사용해야 함
+            <header>
+                <h1>
+                    <a
+                        href="/"
+                        onClick={function (e) {
+                            e.preventDefault();
+                            this.props.onChangePage();
+                        }.bind(this)}
+                    >
+                        {this.props.title}
+                    </a>
+                </h1>
+                {this.props.sub}
+            </header>
+        );
+    }
+}
+
+export default Subject;
