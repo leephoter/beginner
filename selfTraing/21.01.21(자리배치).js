200bcbc16afa50ae9dcc9c0314dca4c313941505
LeePhoter <estarg51@gmail.com>
2021-01-21 22:43:37 +0900

    css. 글자꾸미기

--- selfTraing/21.01.21(자리배치).js
+++ selfTraing/21.01.21(자리배치).js
@@ -1,9 +1,15 @@
 var widthNumber = document.querySelector(".inputWidth");
 var heightNumber = document.querySelector(".inputHeight");
 var shuffleButton = document.querySelector(".shuffleButton");
-var table = document.querySelector("table");
-var newspace = document.createElement("span");
+var table = document.getElementById("table");
+var students = document.getElementById("students");
 
 shuffleButton.addEventListener("click", function () {
-    console.log(widthNumber.value * heightNumber.value);
+    for (var i = 1; i < widthNumber.value * heightNumber.value; i++) {
+        var newspace = document.createElement("span");
+        newspace.className = i;
+        newspace.innerHTML = i;
+        // newspace.classList.add('')
+        students.appendChild(newspace);
+    }
 });
