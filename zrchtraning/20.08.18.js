f7f95e57ffc97baa9208fb2b531ed1edb375e5d9
LeePhoter <estarg51@gmail.com>
2021-02-20 19:13:56 +0900

    객체연습

--- zrchtraning/20.08.18.js
+++ zrchtraning/20.08.18.js
@@ -1,10 +1,22 @@
 var leephoter = {
-    name : 'Hangyeol Lee',
-    food : function food() {
-        console.log('Pork cutlet !!')
-    }
-}
-console.log(leephoter)
-console.log(leephoter.name)
-console.log(leephoter['name'])
-console.log(leephoter.food)
+  name: 'Hangyeol Lee',
+  food: function feed() {
+    console.log('Pork cutlet !!');
+  },
+};
+console.log(leephoter);
+console.log(leephoter.name);
+console.log(leephoter['name']);
+console.log(leephoter.food);
+
+var document = {
+  이름: '이한결',
+  키: 170,
+  몸무게: 70,
+  음식: function 먹다() {
+    console.log('냠냠');
+  },
+};
+console.log(document.이름);
+console.log(document['키']);
+console.log(document.음식);
