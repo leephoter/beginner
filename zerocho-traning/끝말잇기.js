bf91b0b69540a403be638f103f91d1f68a7a6c52
LeePhoter <estarg51@gmail.com>
2021-06-12 17:46:04 +0900

    끝말잇기 게임

--- zerocho-traning/끝말잇기.js
+++
@@ -1,9 +0,0 @@
-var word = prompt("Let's Play the Word Game !!")
-for (var answer = prompt(word); word[word.length - 1] === answer[0]; ) {
-    if (word[word.length - 1] === answer[0]) {
-        alert('nice')
-        word = answer
-        var answer = prompt(word)
-    }
-} alert('U lost !')
-
